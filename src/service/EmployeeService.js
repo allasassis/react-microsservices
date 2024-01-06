c8edb4aedaa489d1acf2249447af4930a35a4245
Allas Assis <allasassis10@gmail.com>
2024-01-06 16:12:52 -0300

    EmployeeService file created with axios getEmployee method

---
+++ src/service/EmployeeService.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const EMPLOYEE_SERVICE_BASE_URL = "http://localhost:9191/api/employee";
+const EMPLOYEE_ID = 5;
+
+class EmployeeService {
+    getEmployee() {
+        axios.get(EMPLOYEE_SERVICE_BASE_URL + "/" + EMPLOYEE_ID)
+    }
+}
+
+export default new EmployeeService
